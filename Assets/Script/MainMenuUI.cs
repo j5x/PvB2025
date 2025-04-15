79077e533cb4675ab57e38a9310305f28d3f89e5
j5x <90614203+j5x@users.noreply.github.com>
2025-04-15 10:54:06 +0200

    MainMenu done

---
+++ Assets/Script/MainMenuUI.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private GameObject creditsPanel;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainLvl");
+    }
+
+    public void ShowCredits()
+    {
+        creditsPanel.SetActive(true);
+    }
+
+    public void HideCredits()
+    {
+        creditsPanel.SetActive(false);
+    }
+}
