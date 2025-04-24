453ea1c7afdde9ee7f1a6e80087c8086d5711f9d
j5x <90614203+j5x@users.noreply.github.com>
2025-04-24 18:39:05 +0200

    Fixed healthbars, both enemy and player healthbars react to dmg and healing, slight adjustment to balancing

--- Assets/Script/UI/HealthBarUI.cs
+++ Assets/Script/UI/HealthBarUI.cs
@@ -3,46 +3,55 @@ using UnityEngine.UI;
 
 public class HealthBarUI : MonoBehaviour
 {
-    [SerializeField] private HealthComponent healthComponent;
     [SerializeField] private Slider slider;
     [SerializeField] private float smoothSpeed = 5f;
 
+    private HealthComponent targetHealthComponent;
     private float targetHealth;
-
-    private void Awake()
-    {
-        if (healthComponent == null)
-            healthComponent = GetComponentInParent<HealthComponent>();
-
-        if (slider == null)
-            slider = GetComponentInChildren<Slider>();
-
-        if (healthComponent != null)
-            healthComponent.OnHealthChanged += OnHealthChanged;
-    }
 
     private void Start()
     {
-        if (healthComponent != null)
-        {
-            slider.maxValue = healthComponent.MaxHealth;
-            slider.value = healthComponent.CurrentHealth;
-            targetHealth = slider.value;
-        }
+        FindEnemyTarget();
     }
 
     private void Update()
     {
+        if (targetHealthComponent == null)
+        {
+            FindEnemyTarget(); // Try again if enemy was destroyed and we need to find a new one
+            return;
+        }
+
         if (Mathf.Abs(slider.value - targetHealth) > 0.01f)
         {
             slider.value = Mathf.Lerp(slider.value, targetHealth, Time.deltaTime * smoothSpeed);
         }
     }
 
+    private void FindEnemyTarget()
+    {
+        GameObject enemy = GameObject.FindWithTag("Enemy");
+        if (enemy != null)
+        {
+            targetHealthComponent = enemy.GetComponent<HealthComponent>();
+
+            if (targetHealthComponent != null)
+            {
+                slider.maxValue = targetHealthComponent.MaxHealth;
+                slider.value = targetHealthComponent.CurrentHealth;
+                targetHealth = slider.value;
+
+                targetHealthComponent.OnHealthChanged += OnHealthChanged;
+            }
+        }
+    }
+
     private void OnDestroy()
     {
-        if (healthComponent != null)
-            healthComponent.OnHealthChanged -= OnHealthChanged;
+        if (targetHealthComponent != null)
+        {
+            targetHealthComponent.OnHealthChanged -= OnHealthChanged;
+        }
     }
 
     private void OnHealthChanged(int currentHealth)
