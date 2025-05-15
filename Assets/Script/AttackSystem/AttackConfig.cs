7b87ac1048bc2d0a3f57b4d12157d0b08e61759f
PurpleGRStudios <133716195+PurpleGRStudios@users.noreply.github.com>
2025-05-15 12:55:26 +0200

    updated AttackConfig and removed hideinspector

--- Assets/Script/AttackSystem/AttackConfig.cs
+++ Assets/Script/AttackSystem/AttackConfig.cs
@@ -4,7 +4,7 @@ using UnityEngine.Serialization;
 [CreateAssetMenu(fileName = "AttackConfig", menuName = "Enemies/AttackConfig")]
 public class AttackConfig : ScriptableObject
 {
-    [HideInInspector] public string attackName = "Default Attack";
+    public string attackName = "Default Attack";
     public int attackDamage = 10;
     [HideInInspector] int attackInterval = 5; // Time between attacks -> Currently controlled by enemy.cs
     public float attackDelay = 0.5f; // Delay after animation starts
